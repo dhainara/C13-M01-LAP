dcc3825d96aae8843e0734a48eebb895960f1ed3
dhainara <daynaralinnyker@gmail.com>
2022-04-03 19:26:41 -0300

    final commit

---
+++ M01-aulas/semana_01/aula_03/aula_03-teoria.js
@@ -0,0 +1,20 @@
+console.clear();
+
+
+//false porque é uma string, ou seja, nao é a mesma coisa que um number
+let comparacao1 = 3 === '3';
+console.log(comparacao1);
+console.log(typeof comparacao1);
+
+console.log();
+
+let comparacao2 = 3 === 3;
+console.log(comparacao2);
+console.log(typeof comparacao2);
+
+console.log();
+
+//verdadeiro, operação está correta
+let comparacao3 = 6 < 9;
+console.log(comparacao3);
+console.log(typeof comparacao3);
