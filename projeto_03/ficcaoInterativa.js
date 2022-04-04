472d22d072997f746d8211c389ff63ad305321ec
dhainara <daynaralinnyker@gmail.com>
2022-04-04 16:19:18 -0300

    test commit

--- projeto_03/ficcaoInterativa.js
+++ projeto_03/ficcaoInterativa.js
@@ -1,3 +1,12 @@
 console.clear();
 const prompt = require('prompt-sync');
 
+let nome = prompt(`Olá, qual o seu nome?`);
+let valida = string === nome;
+console.log(valida);
+
+while (valida) {
+    console.log('Por favor, digite seu nome novamente, e lembresse, ele deve possuir apenas letras.');
+    nome = prompt(`Qual o seu nome?`);
+    valida = string === nome;
+}
